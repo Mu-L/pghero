a51e9a7f5cd6c11bcf3dad1029af1ecc29b658b1
Andrew Kane <andrew@ankane.org>
2026-07-24 12:39:52 -0700

    Dropped support for Sprockets < 4 [skip ci]

--- lib/pghero/engine.rb
+++ lib/pghero/engine.rb
@@ -5,16 +5,9 @@ module PgHero
     initializer "pghero", group: :all do |app|
       # check if Rails api mode
       if app.config.respond_to?(:assets) && defined?(Sprockets)
-        if Sprockets::VERSION.to_i >= 4
-          app.config.assets.precompile << "pghero/application.js"
-          app.config.assets.precompile << "pghero/application.css"
-          app.config.assets.precompile << "pghero/favicon.png"
-        else
-          # use a proc instead of a string
-          app.config.assets.precompile << proc { |path| path == "pghero/application.js" }
-          app.config.assets.precompile << proc { |path| path == "pghero/application.css" }
-          app.config.assets.precompile << proc { |path| path == "pghero/favicon.png" }
-        end
+        app.config.assets.precompile << "pghero/application.js"
+        app.config.assets.precompile << "pghero/application.css"
+        app.config.assets.precompile << "pghero/favicon.png"
       end
 
       file_config = PgHero.file_config || {}
